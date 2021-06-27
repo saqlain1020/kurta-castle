0038b04f7654277a6a5caf638f45bd3a7ccabbab
Saqlain1020 <saqlainprinters@gmail.com>
2021-06-27 10:25:10 +0500

    package updates and component update

---
+++ src/Components/CustomGallery/CustomGallery.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { Grid, makeStyles } from "@material-ui/core";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import PropTypes from "prop-types";
+
+const useStyles = makeStyles((theme) => ({
+  heading: {},
+  imageWrapper: {
+    height: 350,
+    boxShadow: "0px 5px 15px rgba(0,0,0,0.2)",
+    position: "relative",
+    "& img": {
+      objectFit: "cover",
+      borderRadius: 5,
+      transition: "all 200ms ease-in-out",
+    },
+    "&:hover": {
+      "& > div > img": {
+        transform: "scale(1.2)",
+      },
+    },
+  },
+  blurImage: {
+    position: "absolute",
+    borderRadius: 5,
+    filter: "blur(12px)",
+    top: 12,
+    opacity: 0.5,
+    left: 0,
+    zIndex: -1,
+  },
+}));
+
+const CustomGallery = ({ data }) => {
+  const classes = useStyles();
+  const [currentImage, setCurrentImage] = React.useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = React.useState(false);
+
+  const openLightbox = React.useCallback((index) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+  return (
+    <div>
+      <Grid container spacing={2}>
+        {data.map((item, index) => (
+          <Grid item xs={6} sm={4} md={3}>
+            <div className={classes.imageWrapper}>
+              <img
+                src={item.src}
+                width="100%"
+                height="100%"
+                className={classes.blurImage}
+              />
+              <div
+                style={{
+                  height: "100%",
+                  overflow: "hidden",
+                  borderRadius: 5,
+                }}
+              >
+                <img
+                  src={item.src}
+                  width="100%"
+                  height="100%"
+                  onClick={() => openLightbox(index)}
+                />
+              </div>
+            </div>
+          </Grid>
+        ))}
+      </Grid>
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={data.map((x) => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
+};
+
+export default CustomGallery;
+
+CustomGallery.propTypes = {
+  data: PropTypes.array,
+};
+
+CustomGallery.defaultProps = {
+  data: [
+    {
+      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    },
+    {
+      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    },
+    {
+      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    },
+    {
+      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    },
+    {
+      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    },
+    {
+      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    },
+    {
+      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    },
+    {
+      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    },
+    {
+      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    },
+  ],
+};
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { Grid, makeStyles, Typography } from "@material-ui/core";
-import "photoswipe/dist/photoswipe.css";
-import "photoswipe/dist/default-skin/default-skin.css";
-import { Gallery as GallerySwipe, Item } from "react-photoswipe-gallery";
 import { Container } from "@material-ui/core";
 import Img1 from "src/Assets/images/1.jpg";
 import Img2 from "src/Assets/images/2.jpg";
@@ -10,60 +7,19 @@ import Img3 from "src/Assets/images/3.jpg";
 import Img4 from "src/Assets/images/4.jpg";
 import Img5 from "src/Assets/images/5.jpg";
 import ReactGallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
 import data from "./data";
 import clsx from "clsx";
+import CustomGallery from "src/Components/CustomGallery/CustomGallery";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: 30,
   },
-  img: {
-    width: 300,
-    margin: 5,
-  },
-  heading: {},
-  imageWrapper: {
-    height: 350,
-    // overflow: "hidden",
-    boxShadow: "0px 5px 15px rgba(0,0,0,0.2)",
-    position: "relative",
-    "& img": {
-      objectFit: "cover",
-      borderRadius: 10,
-      transition: "all 200ms ease-in-out",
-    },
-    "&:hover": {
-      "& > div > img": {
-        transform: "scale(1.2)",
-      },
-    },
-  },
-  blurImage: {
-    position: "absolute",
-    borderRadius: 10,
-    filter: "blur(12px)",
-    top: 12,
-    opacity: 0.5,
-    left: 0,
-    zIndex: -1,
-  },
 }));
 
 const Gallery = () => {
   const classes = useStyles();
-  const [currentImage, setCurrentImage] = React.useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = React.useState(false);
 
-  const openLightbox = React.useCallback((index) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
   return (
     <div className={classes.root}>
       <Container maxWidth="lg">
@@ -73,49 +29,8 @@ const Gallery = () => {
         >
           Our Best Collections
         </Typography>
-        <div style={{ marginTop: 20 }}>
-          <Grid container spacing={2}>
-            {data.map((item, index) => (
-              <Grid item xs={6} sm={4} md={3}>
-                <div className={classes.imageWrapper}>
-                  <img
-                    src={item.src}
-                    width="100%"
-                    height="100%"
-                    className={classes.blurImage}
-                  />
-                  <div
-                    style={{
-                      height: "100%",
-                      overflow: "hidden",
-                      borderRadius: 10,
-                    }}
-                  >
-                    <img
-                      src={item.src}
-                      width="100%"
-                      height="100%"
-                      onClick={() => openLightbox(index)}
-                    />
-                  </div>
-                </div>
-              </Grid>
-            ))}
-          </Grid>
-          <ModalGateway>
-            {viewerIsOpen ? (
-              <Modal onClose={closeLightbox}>
-                <Carousel
-                  currentIndex={currentImage}
-                  views={data.map((x) => ({
-                    ...x,
-                    srcset: x.srcSet,
-                    caption: x.title,
-                  }))}
-                />
-              </Modal>
-            ) : null}
-          </ModalGateway>
+        <div style={{ marginTop: 40 ,marginBottom:50,}}>
+          <CustomGallery data={data}/>
         </div>
       </Container>
     </div>
